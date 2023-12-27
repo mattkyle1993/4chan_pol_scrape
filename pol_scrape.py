6adf760b07caed52f592f961340b782c6d4ab119
mattkyle1993 <104584170+mattkyle1993@users.noreply.github.com>
2023-12-27 01:15:17 -0600

    fixing issues

--- pol_scrape.py
+++ pol_scrape.py
@@ -16,51 +16,13 @@ from mysql_database_functions import *
 from word_queries_input import *
 import re
 
-<<<<<<< HEAD
-# list of specific words to search for and find matches using fuzzy wuzzy
-
-word_query_list = ["nigger","kike","jew","white","jesus",
-                   "hitler","christian","muslim","troon",
-                   "tranny","genocide","kill","goy",
-                   "globalist","fren","comfy","globohomo",
-                   "pogrom","society","collapse",
-                   "blood","kosher","vermin","military","pajeet",
-                   "shitskin"] # e.g., n-word, k-word, etc.
-
-        
-        
-=======
->>>>>>> testing
 if __name__ == "__main__":
     main = MainScrapeFunc()
     main.minimize_or_hide(minimize=True)
     main.main_function(
-<<<<<<< HEAD
-                        word_query_list=word_query_list,
-                        complete_run=True,
-                        save_thread_list=True,
-                        sql_insert=False, 
-                        # random_grab=3
-                       )
-
-# if there are strange discrepancies in the data from scrape to scrape (within hours), it 
-# could be that previous threads in other scrapes have disappeared (404'd), and so 
-# those word counts went down with them
-
-# /html/body/form[2]/div[1]/div[1]/div[7]/div[2]/div[3]/a/img
-# /html/body/form[2]/div[1]/div[1]/div[2]/div[2]/div[3]/a/img
-
-# Las vegas December 6th (?), 2023 mass shooting 4chan/pol/ responses:
-#         # "https://boards.4chan.org/pol/thread/451088072",
-#         # "https://boards.4chan.org/pol/thread/451086113",
-#         # "https://boards.4chan.org/pol/thread/451090789",
-#         # "https://boards.4chan.org/pol/thread/451088971",
-#         # "https://boards.4chan.org/pol/thread/451092402"
-=======
                         word_query_list=[],
                         complete_run=True,
                         save_thread_list=True,
                         sql_insert=False, 
                         grab=15
                        )
->>>>>>> testing
